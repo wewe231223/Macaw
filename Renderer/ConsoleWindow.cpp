f4f08ec93fd2dc7056719f7eb973cf47031d57f6
권혜원 <130565897+12e0hw@users.noreply.github.com>
2026-09-05 16:49:32 +0900

    feat: command

--- Renderer/ConsoleWindow.cpp
+++ Renderer/ConsoleWindow.cpp
@@ -2,6 +2,12 @@
 #include "ConsoleWindow.h"
 #include "ImGui/imgui.h"
 
+#include <sstream>
+
+// 로그 저장 시
+using enum ELogLevel;
+using enum ELogCategory;
+
 namespace
 {
     const char* LogCategoryToString(ELogCategory Category)
@@ -15,12 +21,66 @@ namespace
         default:                    return "[Unknown]";
         }
     }
+
+    void ExecuteCommand(FConsoleOutputHandle Handle, const char* Input)
+    {
+        std::istringstream Stream(Input);
+
+        FString Command;
+        Stream >> Command;
+
+        if (Command == "clear")
+        {
+            Console::Clear(Handle);
+        }
+        else if (Command == "help")
+        {
+            Console::AddLog(
+                Handle,
+                Log,
+                Core,
+                "Commands: clear, help, echo");
+        }
+        else if (Command == "echo")
+        {
+            FString Text;
+            std::getline(Stream >> std::ws, Text);
+
+            Console::AddLog(
+                Handle,
+                Log,
+                Core,
+                "> %s",
+                Text.c_str());
+        }
+        else
+        {
+            Console::AddLog(
+                Handle,
+                Warning,
+                Core,
+                "Unknown command: %s",
+                Command.c_str());
+        }
+    }
 }
 
 void DrawConsole(FConsoleOutputHandle Handle)
 {
     ImGui::Begin("Console");
     const size_t Count = Console::GetMessageCount(Handle);
+
+    // 로그 영역
+    float FooterHeight =
+        ImGui::GetFrameHeightWithSpacing();
+
+    ImGui::BeginChild(
+        "LogRegion",
+        ImVec2(0, -FooterHeight),
+        true
+    );
+
+    Console::Flush(Handle);
 
     for (size_t Index = 0; Index < Count; ++Index)
     {
@@ -64,6 +124,36 @@ void DrawConsole(FConsoleOutputHandle Handle)
         ImGui::TextUnformatted(Message.Text.c_str());
     }
 
+    ImGui::SetScrollHereY(1.0f);
+
+    ImGui::EndChild();
+
+    static char InputBuf[256] = "";
+
+    ImGui::Separator();
+
+    if (ImGui::InputText(
+        "##ConsoleInput",
+        InputBuf,
+        sizeof(InputBuf),
+        ImGuiInputTextFlags_EnterReturnsTrue))
+    {
+        if (InputBuf[0] != '\0')
+        {
+            /*Console::AddLog(
+                Console::STDOutHandle,
+                ELogLevel::Log,
+                ELogCategory::Core,
+                "> %s",
+                InputBuf);*/
+
+            ExecuteCommand(Console::STDOutHandle, InputBuf);
+            InputBuf[0] = '\0';
+
+            ImGui::SetKeyboardFocusHere(-1);
+        }
+
+    }
     ImGui::End();
 }
 
