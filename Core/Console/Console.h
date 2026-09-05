518cce3c335e18b853dd1f2325190423eb4df759
권혜원 <130565897+12e0hw@users.noreply.github.com>
2026-09-05 15:17:00 +0900

    feat: ConsoleMessage 가변인자 처리

---
+++ Core/Console/Console.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include "pch.h"
+
+enum class ELogLevel { Log, Warning, Error, Fatal };
+enum class ELogCategory { Core, Render, Physics, Etc };
+
+struct FConsoleMessage
+{
+    FString Time;
+    ELogLevel Level = ELogLevel::Log;  
+    ELogCategory Category = ELogCategory::Etc;
+    FString Text;
+};
+
+struct FConsoleOutputHandle
+{
+    uint32_t Index = UINT32_MAX;
+
+    bool IsValid() const
+    {
+        return Index != UINT32_MAX;
+    }
+};
+
+namespace Console
+{
+    inline constexpr FConsoleOutputHandle STDOutHandle{ 0 };
+    inline constexpr FConsoleOutputHandle STDErrorHandle{ 1 };
+
+    void Print(FConsoleOutputHandle Handle, FConsoleMessage Message);
+    void AddLog(
+        FConsoleOutputHandle Handle,
+        ELogLevel Level,
+        ELogCategory Categor,
+        const char* Format,
+        ...);
+    size_t GetMessageCount(FConsoleOutputHandle Handle);
+    const FConsoleMessage& GetMessageAt(FConsoleOutputHandle Handle, size_t Index);
+}
+
+
+
