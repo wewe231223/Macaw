79325f7d2d88482a5bd5a1bc48fe0127840f83c1
SeungBeom Kim <64590456+wewe231223@users.noreply.github.com>
2026-09-08 17:48:32 +0900

    Update FMouseInput.h

--- FMouseInput.h
+++ FMouseInput.h
@@ -7,6 +7,10 @@
 
 #include "Core/Channel/FMessageChannel.h"
 
+enum EMouseSide : uint32 {
+    Left, Right, MAX
+};
+
 class FMouseInput
 {
 public:
@@ -37,4 +41,17 @@ private:
 
     float PendingRotateDeltaX = 0.0f;
     float PendingRotateDeltaY = 0.0f;
+
+    struct DragCapture {
+        POINT start;
+        POINT current; 
+    };
+
+	TFixedArray<DragCapture, static_cast<size_t>(EMouseSide::MAX)> ClickCaptures{};
+
+
+	DragCapture[Left].start = ....;
+    ClickCaptures[Left].current = .....;
+
+
 };
